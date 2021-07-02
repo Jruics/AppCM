5f364a060da38b2ed96ba5b8c3a627ad3d71bff2
João Sousa <paleoloth@gmail.com>
2021-07-02 19:19:50 +0100

    Criação de uma página de boas vindas com botão para criação de notas offline

---
+++ app/src/main/java/ipvc/estg/appcm/HomeActivity.kt
@@ -0,0 +1,18 @@
+package ipvc.estg.appcm
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+
+    fun offlineNotes(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}

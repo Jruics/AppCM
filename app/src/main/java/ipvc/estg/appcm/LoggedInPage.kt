bcb2b0bcb7f21bfd19e214447fbfaf98df1eee50
João Sousa <paleoloth@gmail.com>
2021-07-04 16:59:54 +0100

    Adição de páginas para login(LoginPage) e receção de login(LoggedInPage).
    Adição de verificação de login, assim como os endpoints relevantes para o mesmo.
    
    Criação do ServiceBuilder

--- app/src/main/java/ipvc/estg/appcm/LoggedInPage.kt
+++ app/src/main/java/ipvc/estg/appcm/LoggedInPage.kt
@@ -15,39 +15,7 @@ class LoggedInPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in_menu)
-        /*
-        shared_preferences = getSharedPreferences("shared_preferences", Context.MODE_PRIVATE)
-        val username = shared_preferences.getString("username", "")
-        textWelcome = findViewById(R.id.welcome)
-        var welcome = getString(R.string.welcome)
-        textWelcome.text = "$welcome " + "$username"
-        */
+
     }
 
-    /*
-    fun problemas_lista(view: View){
-        val intent = Intent(this@Menu, Problema_List::class.java)
-        startActivity(intent)
-    }
-
-    fun problemas_mapa(view: View){
-        val intent = Intent(this@Menu, MapsActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun problemas_reportar(view: View){
-        val intent = Intent(this@Menu, Problema_Reportar::class.java)
-        startActivity(intent)
-    }
-
-    fun logout(view: View){
-        val shared_preferences_edit : SharedPreferences.Editor = shared_preferences.edit()
-        shared_preferences_edit.clear()
-        shared_preferences_edit.apply()
-
-        val intent = Intent(this@Menu, Login::class.java)
-        startActivity(intent)
-        finish()
-    }
-    */
 }
